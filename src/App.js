86e90be972bb936b244558fd5efbc023d015773d
maxwell <mtkatombo@gmail.com>
2023-07-05 01:38:35 +0200

    shopping page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
 import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppProvider>
+      <div className="App">
+        <h1 className='mt-3'>Shopping App</h1>
+        <div className='row mt-3'>
+          <div className='col-sm'>
+            <CartValue />
+          </div>
+          <div className='col-sm'>
+            <Location />
+          </div>
+        </div>
+        <h3 className='mt-3'>Shopping Cart</h3>
+        <div className='row '>
+          <div className='col-sm'>
+            < ExpenseList />
+          </div>
+        </div>
+        <h3 className='mt-3'>Add Items</h3>
+        <div className='row mt3'>
+          <div className='col-sm'>
+            <ItemSelected />
+          </div>
+        </div>
+      </div >
+    </AppProvider>
   );
 }
 
